85b76c6487eb64b68fe8d3d5c4e4c3f992344dcf
sajan1789 <sajan99559@gmail.com>
2024-11-17 00:59:55 +0530

    added

--- src/Page/TurnkeySolution.jsx
+++ src/Page/TurnkeySolution.jsx
@@ -27,35 +27,36 @@ const TurnkeySolution = () => {
   }
 
   return (
-    <div className="text-white flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8 ">
-      {turnKeyData.map((item, index) => (
-        <div
-          key={index}
-          className={`flex flex-col lg:flex-row items-center mb-10 ${
-            index % 2 === 0 ? "lg:flex-row lg:px-[120px]" : "lg:flex-row-reverse marker:"
-           } w-full `}
-        >
-          {/* Title and Description Section */}
-          <div className="lg:w-1/2 flex flex-col justify-center items-center lg:items-start text-center lg:text-left mb-6 lg:mb-0 px-4">
-            <h1 className="text-[28px] sm:text-[36px] md:text-[48px] lg:text-[64px] font-bold leading-tight">
-              {item.title}
-            </h1>
-            <p className="text-[14px] sm:text-[17px] md:text-[20px] mt-4 leading-relaxed font-light lg:mr-2">
-              {item.description}
-            </p>
-          </div>
-
-          {/* Image Section */}
-          <div className="lg:w-1/2 flex justify-center px-4">
-            <img
-              src={item.image}
-              alt={`${item.title} Graphic`}
-              className="w-[90%] sm:w-[80%] lg:w-[75%] h-auto object-contain"
-            />
-          </div>
+    <div className="text-white flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8 mt-20 space-y-10">
+    {turnKeyData.map((item, index) => (
+      <div
+        key={index}
+        className={`flex flex-col lg:flex-row items-center ${
+          index % 2 === 0 ? "lg:flex-row lg:px-[120px]" : "lg:flex-row-reverse"
+        } w-full`}
+      >
+        {/* Title and Description Section */}
+        <div className="lg:w-1/2 flex flex-col justify-center items-center lg:items-start text-center lg:text-left mb-6 lg:mb-0 px-4">
+          <h1 className="text-[28px] sm:text-[36px] md:text-[48px] lg:text-[64px] font-bold leading-tight">
+            {item.title}
+          </h1>
+          <p className="text-[14px] sm:text-[17px] md:text-[20px] mt-4 leading-relaxed font-light lg:mr-2">
+            {item.description}
+          </p>
         </div>
-      ))}
-    </div>
+  
+        {/* Image Section */}
+        <div className="lg:w-1/2 flex justify-center px-4">
+          <img
+            src={item.image}
+            alt={`${item.title} Graphic`}
+            className="w-[90%] sm:w-[80%] lg:w-[75%] h-auto object-contain"
+          />
+        </div>
+      </div>
+    ))}
+  </div>
+  
   );
 };
 
